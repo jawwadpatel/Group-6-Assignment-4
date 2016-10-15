cadbf4eb846d4b0fb4f23e396a664d060fc3b0d4
Jawwad Patel <jawwadmemon89@gmail.com>
2016-10-15 20:38:29 +1100

    Final Debugged Code
    This file contains all the changes we made for this code debugging
    project.

---
+++ src/Dice.java
@@ -0,0 +1,21 @@
+public class Dice {
+			
+	private DiceValue value;
+	
+	public Dice() {
+		value =  DiceValue.getRandom();
+	}
+	
+	public DiceValue getValue() {
+		return value;
+	}
+
+	public DiceValue roll() {
+		this.value = DiceValue.getRandom();
+		return this.value;
+	}		
+	
+	public String toString() {
+		return value.toString();
+	}
+}
